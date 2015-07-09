7ddf3332a8431ee950d03863dc8b02db4c3aa9b0
Phil Wright <touchopia@me.com>
2015-07-09 12:27:17 -0600

    Adding all files

---
+++ Tester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tester
+//
+//  Created by Phil Wright on 7/9/15.
+//  Copyright (c) 2015 Touchopia LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
